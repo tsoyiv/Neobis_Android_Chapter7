cdc9f3d73a84dcd1a1447fb5475ca81227a68011
Vlad <tsoiivv@gmail.com>
2023-05-29 16:53:50 +0600

    added first logic for login page

--- app/src/main/java/com/example/my_app_seven/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/my_app_seven/fragments/LoginFragment.kt
@@ -1,6 +1,8 @@
 package com.example.my_app_seven.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -25,11 +27,30 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         toResetPassword()
-    }
+        checkInput()
 
+    }
     private fun toResetPassword() {
         binding.textForgetPsw.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_resetPasswordFragment)
+        }
+    }
+    private fun checkInput() {
+        binding.inputLoginEmail.addTextChangedListener(inputTextWatcher)
+        binding.inputLoginPassword.addTextChangedListener(inputTextWatcher)
+    }
+    private val inputTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val usernameInput = binding.inputLoginEmail.text.toString().trim()
+            val passwordInput = binding.inputLoginPassword.text.toString().trim()
+
+            binding.loginButton.isEnabled = !usernameInput.isEmpty() && !passwordInput.isEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable?) {
         }
     }
 }
