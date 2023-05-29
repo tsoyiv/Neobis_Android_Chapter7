69b8a223cade3da445dcf19ade5e904b667508fc
Vlad <tsoiivv@gmail.com>
2023-05-29 17:20:18 +0600

    finished ui of login page

--- app/src/main/java/com/example/my_app_seven/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/my_app_seven/fragments/LoginFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.my_app_seven.R
 import com.example.my_app_seven.databinding.FragmentLoginBinding
@@ -28,7 +29,6 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         toResetPassword()
         checkInput()
-
     }
     private fun toResetPassword() {
         binding.textForgetPsw.setOnClickListener {
@@ -42,14 +42,20 @@ class LoginFragment : Fragment() {
     private val inputTextWatcher = object : TextWatcher {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
         }
-
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             val usernameInput = binding.inputLoginEmail.text.toString().trim()
             val passwordInput = binding.inputLoginPassword.text.toString().trim()
 
-            binding.loginButton.isEnabled = !usernameInput.isEmpty() && !passwordInput.isEmpty()
-        }
+            val button = binding.loginButton
 
+            if (usernameInput.isEmpty() || passwordInput.isEmpty()) {
+                button.isEnabled = false
+                button.setBackgroundResource(R.drawable.button_grey)
+            } else {
+                button.isEnabled = true
+                button.setBackgroundResource(R.drawable.rounded_btn)
+            }
+        }
         override fun afterTextChanged(s: Editable?) {
         }
     }
