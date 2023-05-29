9325047f20415b7c87f1a8d6ea223ab5df442b11
Vlad <tsoiivv@gmail.com>
2023-05-29 15:00:40 +0600

    added ui design and new fragments

--- app/src/main/java/com/example/my_app_seven/fragments/SecondResetPasswordFragment.kt
+++ app/src/main/java/com/example/my_app_seven/fragments/SecondResetPasswordFragment.kt
@@ -5,31 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.example.my_app_seven.R
-import com.example.my_app_seven.databinding.FragmentResetPasswordBinding
+import com.example.my_app_seven.databinding.FragmentSecondResetPasswordBinding
 
-class ResetPasswordFragment : Fragment() {
 
-    private lateinit var binding: FragmentResetPasswordBinding
+class SecondResetPasswordFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondResetPasswordBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentResetPasswordBinding.inflate(inflater, container, false)
+        binding = FragmentSecondResetPasswordBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        toLoginPage()
-    }
 
-    private fun toLoginPage() {
-        binding.returnLoginPageBtn.setOnClickListener {
-            findNavController().navigate(R.id.action_resetPasswordFragment_to_loginFragment)
-        }
     }
 }
